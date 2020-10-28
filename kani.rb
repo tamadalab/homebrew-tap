956b1282a5657689116c30d00aa258f60e3e08fa
masa <36505606+ma-sa321@users.noreply.github.com>
2020-10-28 23:14:28 +0900

    add License

--- kani.rb
+++ kani.rb
@@ -4,10 +4,8 @@ HOMEBREW_KANI_VERSION = "1.0.0"
 class Kani < Formula
   desc "これは，git初心者向けのcommitを促す補助ツールになります．git addされる前とのファイル編集行と，コマンド操作時の連続エラー発生回数によってcommitを促すか否かを判別するツールです．促すべきと判断した場合，git addやstatusなどの説明をターミナル上に簡易に表示します．"
   homepage "https://github.com/tamadalab/kani"
-  # ライセンスを決めましょう．適当に決めてもらってOKです．
-  license ""
-  # 将来的には tag を付けた方が良い．URLは自分のものに置き換えてください．
-  # url "https://github.com/ma-sa321/kani.git", :tag => "v#{HOMEBREW_KANI_VERSION}"
+  license "CC0"
+  # url "https://github.com/tamadalab/kani.git", :tag => "v#{HOMEBREW_KANI_VERSION}"
   url "https://github.com/tamadalab/kani.git", :branch => "master"
   version HOMEBREW_KANI_VERSION
   head "https://github.com/tamadalab/kani.git"
