563f70962ebfa0b765e69eb62b69062b7aea3e08
Haruaki Tamada <tamada@users.noreply.github.com>
2021-01-26 10:42:04 +0900

    release purplecat v0.3.3

--- purplecat.rb
+++ purplecat.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-HOMEBREW_PURPLECAT_VERSION = "0.1.0"
+HOMEBREW_PURPLECAT_VERSION = "0.3.3"
 
 class Purplecat < Formula
   desc "Purple cat, Purple cat, What do you see? I see the dependant libraries and their licenses!"
@@ -7,9 +7,12 @@ class Purplecat < Formula
   license "WTFPL"
   url "https://github.com/tamadalab/purplecat/releases/download/v#{HOMEBREW_PURPLECAT_VERSION}/purplecat-#{HOMEBREW_PURPLECAT_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_PURPLECAT_VERSION
-  sha256 "5a09f5a057a3082c44190af123dc2b9ccbcefc57a5b3d3f424e5832ee66d8889"
+  sha256 "dcfc80103c79e06affe2bb4668867369a0debb2d7ca1c56b961444d5a9e1f86a"
 
   def install
+    prefix.install "README.md"
+    prefix.install "LICENSE"
+    prefix.install "docs"
     bin.install "bin/purplecat"
 
     if build.with? "completions"
