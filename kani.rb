eca75a5cd393e2b10a2360fc4576ea7d3a9c9c4a
Haruaki Tamada <tamada@users.noreply.github.com>
2021-06-03 14:34:17 +0900

    update kani version 1.0.3

--- kani.rb
+++ kani.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-HOMEBREW_KANI_VERSION = "1.0.1"
+HOMEBREW_KANI_VERSION = "1.0.3"
 
 class Kani < Formula
   desc "This tool supports git/GitHub operations for the novice developers on the shell."
@@ -8,9 +8,10 @@ class Kani < Formula
   # url "https://github.com/tamadalab/kani.git", :tag => "v#{HOMEBREW_KANI_VERSION}"
   url "https://github.com/tamadalab/kani/releases/download/v#{HOMEBREW_KANI_VERSION}/kani-#{HOMEBREW_KANI_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_KANI_VERSION
-  sha256 "b71c856e4bb5c7d7e6b5faebaa418a9f86c42793cb4a70c6bdc5862b96c0f5c0"
+  sha256 "0a0b9d57ec9121202b3d07583c5cf753aeabd416cfd77af28251eb3dfad42a8a"
 
   depends_on "git"
+  depends_on "bash-preexec"
 
   def install
     bin.install "bin/kani"
