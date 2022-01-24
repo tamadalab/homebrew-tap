88d1785ba7ab252c0fa22f5d381607b056511c2d
Haruaki Tamada <tamada@users.noreply.github.com>
2022-01-24 14:10:45 +0900

    fix the invalid script

--- nulete.rb
+++ nulete.rb
@@ -5,7 +5,7 @@ class Nulete < Formula
   desc "Generating unit tests tool for assignments in the Java programming courses"
   homepage "https://github.com/tamadalab/nulete"
   license "CC0-1.0"
-  url "https://github.com/tamadalab/nulete/releases/download/v#{HOMEBREW_NULETE_VERSION}/nulete-#{HOMEBREW_PURPLECAT_VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/tamadalab/nulete/releases/download/v#{HOMEBREW_NULETE_VERSION}/nulete-#{HOMEBREW_NULETE_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_NULETE_VERSION
   sha256 "7d7ade1cb4cf9ce6ce45593e5ad1089af09c375f30ccf17e40bd80cb3c4de893"
 
